a763f64d26a31f423e16cd682655b2dc3ccdbd02
John Jamora <769-jjamora@users.noreply.gitlab1.its.sfu.ca>
2021-02-19 01:16:31 -0800

    Finished Tutorial Video #2 (Templates)

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Sample Blog Post',
+        'content': 'This is the post.',
+        'date_posted': 'January 21, 2021'
+    }, {
+        'author': 'Bob Sanders',
+        'title': 'Alternate Post',
+        'content': 'Hello universe.',
+        'date_posted': 'March 7th, 2018'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
